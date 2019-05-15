6ee377220ef11a6767410014043f6c091b96c202
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-05-15 08:33:56 +0100

    Add Explicit Binding Notes

--- this.js
+++ this.js
@@ -14,3 +14,6 @@
 
 
 //====== Explicit Binding ======//
+// .call() requires arguments to be passed individually
+// .apply() Accepts sing array to pass arguments
+// .bind(): Stores this keyword for function to be called later
