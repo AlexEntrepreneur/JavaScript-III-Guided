57f6d78c1e79cd65568e6236cc1be197ed1fd386
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-12-18 09:26:26 +0000

    .

--- this.js
+++ this.js
@@ -1,13 +1,32 @@
 //====== Global Binding ======//
+// "use strict";
 
+console.log(this);
 
+function whatIsThis() {
+  console.log(this);
+}
 
+whatIsThis();
 
 
 
 //====== Implicit Binding ======//
+// "Automatic" binding
+// When we have a METHOD, this refers to the parent object
+// Don't use arrow functions!
 
+const hobbit = {
+  name: "Samwise",
+  age: 32,
+  food: "taters",
+  cook: function() {
+    console.log(this);
+    return `${this.name} loves to cook ${this.food}`;
+  }
+}
 
+console.log(hobbit.cook());
 
 
 
